a232cdc32c64fa5e34e34c5ad129487b1491ddcb
Rafael Martins <redmajor@gmail.com>
2019-04-06 02:33:30 -0300

    QueryBuffer split in header / implementation files.

---
+++ QueryBuffer.cpp
@@ -0,0 +1,84 @@
+#include "QueryBuffer.h"
+
+#include <sys/time.h>
+
+static double now() {
+	struct timeval tv;
+	gettimeofday(&tv, nullptr);
+	return tv.tv_sec + tv.tv_usec * 1e-6;
+}
+
+double QueryBuffer::block_rate() {
+	std::shared_lock { mutex };
+	return br;
+}
+
+QueryBuffer::QueryBuffer(const long block_size, const long num_entries) : block_size { block_size }, total_size { block_size * num_entries } {
+	data = new unsigned char[total_size];
+	last_block_time = now();
+}
+
+QueryBuffer::~QueryBuffer() {
+	delete[] data;
+}
+
+long QueryBuffer::bs() {
+	return block_size;
+}
+
+bool QueryBuffer::hasSpace() {
+	std::shared_lock { mutex };
+	return used < total_size;
+}
+
+void QueryBuffer::add() {
+	std::unique_lock { mutex };
+
+	double n = now();
+	br = n - last_block_time;
+	last_block_time = n;
+	used += block_size;
+	end = (end + block_size) % total_size;
+
+	if (waiting && used >= waiting_min_data) {
+		waiting = false;
+		has_data.notify_one();
+	}
+}
+
+unsigned char* QueryBuffer::peekFront() {
+	std::shared_lock { mutex };
+	return &data[start];
+}
+
+unsigned char* QueryBuffer::peekEnd() {
+	std::shared_lock { mutex };
+	return &data[end];
+}
+
+void QueryBuffer::consume(int n) {
+	std::unique_lock { mutex };
+	used -= n * block_size;
+	start = (start + n * block_size) % total_size;
+}
+
+bool QueryBuffer::empty() {
+	std::shared_lock { mutex };
+	return used == 0;
+}
+
+int QueryBuffer::entries() {
+	std::shared_lock { mutex };
+	return used / block_size;
+}
+
+void QueryBuffer::waitForData(int n) {
+	std::unique_lock lck { mutex };
+
+	waiting_min_data = n * block_size;
+
+	if (used < waiting_min_data) {
+		waiting = true;
+		has_data.wait(lck, [this] {return ! waiting;});
+	}
+}
