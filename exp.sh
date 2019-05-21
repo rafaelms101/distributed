1ef016953bce36cb1261abd16b6efab6a838eb3f
Rafael Martins <redmajor@gmail.com>
2019-05-21 01:03:04 -0300

    Updated exp script

--- exp.sh
+++ exp.sh
@@ -1,31 +1,27 @@
-for i in {1..10}
+for i in {1..1}
 do
-  echo "Execution #$i"
-  echo "mpirun -n 3 ./sharded b"
+  #echo "Execution #$i"
+  #echo "mpirun -n 3 ./sharded b"
   mpirun -n 3 ./sharded b
 
-  for load in 0.2 0.4 0.6 0.8 1
+  for load in 0.2 0.4 0.6 0.8 1 1.2 2
   do
-    for block_size in 10 20 30 40 50
+    # echo "mpirun -n 3 ./sharded d c $load min"
+    # mpirun -n 3 ./sharded d c $load min
+    # echo "mpirun -n 3 ./sharded d c $load max"
+    # mpirun -n 3 ./sharded d c $load max
+    #
+    echo "d $load min"
+    mpirun -n 3 ./sharded d p $load min
+    echo "d $load max"
+    mpirun -n 3 ./sharded d p $load max
+
+    for block_size in {20..300..20}
     do
-      echo "mpirun -n 3 ./sharded s c $load $block_size"
-      mpirun -n 3 ./sharded s c $load $block_size
+      # echo "mpirun -n 3 ./sharded s c $load $block_size"
+      # mpirun -n 3 ./sharded s c $load $block_size
+      echo "s $load $block_size"
+      mpirun -n 3 ./sharded s p $load $block_size
     done
-
-    echo "mpirun -n 3 ./sharded d c $load min"
-    mpirun -n 3 ./sharded d c $load min
-    echo "mpirun -n 3 ./sharded d c $load max"
-    mpirun -n 3 ./sharded d c $load max
   done
-
-  for block_size in 10 20 30 40 50
-  do
-    echo "mpirun -n 3 ./sharded s p 0.5 $block_size"
-    mpirun -n 3 ./sharded s p 0.5 $block_size
-  done
-
-  echo "mpirun -n 3 ./sharded d p 0.5 min"
-  mpirun -n 3 ./sharded d p 0.5 min
-  echo "mpirun -n 3 ./sharded d p 0.5 max"
-  mpirun -n 3 ./sharded d p 0.5 max
 done
