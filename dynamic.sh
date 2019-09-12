e16d67d6ca57530314eabaad6205e8e2d450a639
Rafael <redmajor@gmail.com>
2019-09-12 20:52:11 -0300

    Changed dynamic script to use loads instead of intervals

--- dynamic.sh
+++ dynamic.sh
@@ -1,29 +1,26 @@
 cmd=mpirun
-total_time=$($cmd -n 3 ./sharded d b 0 g 2 | tail -n 1)
-time=$(bc -l <<< "$total_time / 10000")
 
+echo 'benching'
 $cmd -n 3 ./sharded b
 
-for i in {1..3}
+for i in {1..1}
 do
   for load in 0.2 0.4 0.6 0.8 1
   do
-    interval=$(bc -l <<< "$time / $load")
-
-    for alg in "g" "gmin" "q" "c"
+    for alg in "q" "gmin" "g"
     do
       echo "d c $load $alg"
-      $cmd -n 3 ./sharded d c $interval $alg 2
+      $cmd -n 3 ./sharded d c $load $alg 2
       echo "d p $load $alg"
-      $cmd -n 3 ./sharded d p $interval $alg 2
+      $cmd -n 3 ./sharded d p $load $alg 2
     done
 
-    for block_size in {25..300..25}
+    for block_size in {25..200..25}
     do
       echo "s c $load $block_size"
-      $cmd -n 3 ./sharded s c $interval $block_size 2
+      $cmd -n 3 ./sharded s c $load $block_size 2
       echo "s p $load $block_size"
-      $cmd -n 3 ./sharded s p $interval $block_size 2
+      $cmd -n 3 ./sharded s p $load $block_size 2
     done
   done
 done
