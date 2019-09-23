e3977258ae22ddde79af3a35ab73ce4680145de9
Rafael <redmajor@gmail.com>
2019-09-23 17:27:00 -0300

    batch script

--- batch.sh
+++ batch.sh
@@ -1,11 +1,16 @@
-for i in {1..1}
+cmd=mpirun
+
+declare -A pairs=([1000]=0.7 [2000]=0.7 [2500]=0.68 [5000]=0.64 [10000]=0.6 [25000]=0.6 [50000]=0.54)
+for i in {1..3}
 do
-        echo 'benching'
-        mpirun -n 3 ./sharded b
-        echo 'cpu'
-        mpirun -n 3 ./sharded d c 0 c 2
-        echo 'gpu'
-        mpirun -n 3 ./sharded d c 0 gmin 2
-        echo 'hybrid'
-        mpirun -n 3 ./sharded d c 0 h 2
+	for bs in "${!pairs[@]}"
+	do
+	  ratio=${pairs[$bs]}
+
+	  for alg in "cpu" "gpu" "h $ratio"
+	  do
+	  	echo "$bs $alg"
+	  	$cmd -n 3 ./sharded s b 0 $bs $alg 2
+	  done
+	done
 done
