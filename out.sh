c83b7ee17da3e57a62fac8709de879b5b85a1479
Rafael <redmajor@gmail.com>
2019-09-24 23:41:00 -0300

    Changed cf alg with f alg

--- out.sh
+++ out.sh
@@ -9,7 +9,7 @@ for i in {1..5}
 do
   for load in 0.2 0.4 0.6 0.8 1 1.2
   do
-    for alg in "c" "g 1 4" "g 1 2"  "g 3 4" "cf 1 4" "cf 1 2"  "cf 3 4" "h 1 4" "h 1 2"  "h 3 4" 
+    for alg in "c" "g 1 4" "g 1 2"  "g 3 4" "f 1 4" "f 1 2"  "f 3 4" "h 1 4" "h 1 2"  "h 3 4" 
     do
       echo "c $load $alg"
       $cmd -n 3 ./sharded c $load 2 $alg
