eced9d6d4f9e55b5648ad9fab5d9e98a62bd25db
Rafael <redmajor@gmail.com>
2019-11-27 14:19:26 -0300

    Extra debug info enabled

--- sharded.cpp
+++ sharded.cpp
@@ -159,7 +159,7 @@ static void logSearchStart(int shard) {
 	gethostname(hostname, 1024);
 	int n;
 	cudaGetDeviceCount(&n);
-	deb("%d) Starting at %s. Visible gpus: %d", shard, hostname, n);
+	std::printf("%d) Starting at %s. Visible gpus: %d\n", shard, hostname, n);
 }
 
 int main(int argc, char* argv[]) {
